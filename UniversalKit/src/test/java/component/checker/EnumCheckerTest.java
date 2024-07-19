3b3e0ff1028008df74b9c466ca255f4a0aa4e74b
liuxuanjie <liuxuanjie@kanyun.com>
2024-07-19 18:58:06 +0800

    update struct

--- UniversalKit/src/test/java/component/checker/EnumCheckerTest.java
+++ UniversalKit/src/test/java/component/checker/EnumCheckerTest.java
@@ -4,9 +4,6 @@ import com.romje.component.checker.CheckResult;
 import com.romje.component.checker.EnumChecker;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -16,18 +13,6 @@ public class EnumCheckerTest {
 
     @Test
     public void testCheckRepeat() {
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-        map.put(1, List.of(1, 2, 3));
-        map.put(2, List.of(4, 5, 6));
-        System.out.println(map.toString());
-
-        try {
-            assertTrue(EnumChecker.hasRepeat(HasRepeatEnumTest.class, "code"));
-            assertFalse(EnumChecker.hasRepeat(NonRepeatEnumTest.class, "code"));
-        } catch (Exception e) {
-            fail("Exception should not be thrown: " + e.getMessage());
-        }
-
         try {
             CheckResult checkResult = EnumChecker.checkRepeat(HasRepeatEnumTest.class, "code");
             assertFalse(checkResult.result());
